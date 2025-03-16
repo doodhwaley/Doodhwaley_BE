a9e011ad3c2c6a08854d58461734fcad92b5fafb
Rizwan Rasheed <iamrizwanrasheed@gmail.com>
2025-03-16 21:24:10 +0500

    trying to fix db connection for vercel

--- config/db.js
+++ config/db.js
@@ -1,16 +1,29 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Cache connection for serverless
+let cachedConnection = null;
+
 const connectDB = async () => {
+  // If we have a cached connection, use it
+  if (cachedConnection) {
+    return cachedConnection;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
+
+    cachedConnection = conn;
     console.log("MongoDB Connected...");
+    return conn;
   } catch (err) {
     console.error("Error connecting to MongoDB:", err.message);
-    process.exit(1); // Exit process with failure
+    // Don't exit process in serverless environment - this kills the function
+    // process.exit(1); - REMOVE THIS LINE
+    throw err; // Throw the error instead
   }
 };
 
